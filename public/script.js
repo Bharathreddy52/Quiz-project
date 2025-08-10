2f273a1b4d614fe6b82ce1f0c990d5b9e35a57c0
Bharath <bharathreddyavula5@gmail.com>
2025-08-10 17:25:10 +0530

    first commit

---
+++ public/script.js
@@ -0,0 +1,13 @@
+let obj = {};
+// let createbtn = document.getElementById("createbtn");
+// createbtn.addEventListener("click",()=>{
+//     obj = {};
+// });
+
+let name = document.getElementById("Quiz_name");
+let submitbtn = document.getElementById("submitbtn");
+
+submitbtn.addEventListener("click",()=>{
+    console.log(name.value);
+    fetch()
+});
